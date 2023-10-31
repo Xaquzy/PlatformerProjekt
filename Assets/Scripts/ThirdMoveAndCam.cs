24a8cc7f6950cdb3ead36562233fa40368a6f411
Xaquzy <143174417+Xaquzy@users.noreply.github.com>
2023-10-31 05:45:15 +0100

    Camera fix

--- Assets/Scripts/ThirdMoveAndCam.cs
+++ Assets/Scripts/ThirdMoveAndCam.cs
@@ -64,12 +64,12 @@ public class ThirdMoveAndCam : MonoBehaviour
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
 
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+        if (direction.magnitude >= 0.1f)
+        {
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
         }
